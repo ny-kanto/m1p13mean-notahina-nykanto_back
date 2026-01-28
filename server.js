ea13d75c9f32812aa601aa22d2850d4b3ee2f0fb
Ny Kanto RANDRIAMBOLOLONA <120046452+ny-kanto@users.noreply.github.com>
2026-01-28 09:05:35 +0300

    modify cors

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: 'http://localhost:4200',
+  origin: 'https://m1p13mean-notahina-nykanto-front.netlify.app',
   credentials: true
 }));
 
