5eb20637851fcb90c50310640b4b85c226c0f967
Ny Kanto RANDRIAMBOLOLONA <120046452+ny-kanto@users.noreply.github.com>
2026-02-04 13:22:56 +0300

    Update boutique.controller.js

--- controllers/boutique.controller.js
+++ controllers/boutique.controller.js
@@ -1,17 +1,19 @@
 import Boutique from "../models/boutique.model.js";
-import { pagination } from "../utils/pagination.js";
+// import { pagination } from "../utils/pagination.js";
 
 
 export const getAllBoutiques = async (req, res) => {
-  try {
-    const result = await pagination(
-      Boutique,
-      {},
-      req
-    );
+    try {
+        // const result = await pagination(
+        //   Boutique,
+        //   {},
+        //   req
+        // );
 
-    res.json(result);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
+        // res.json(result);
+        // const boutiques = await Boutique.find();
+        res.json(boutiques);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
 };
