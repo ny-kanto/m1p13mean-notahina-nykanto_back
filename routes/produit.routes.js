441b9d11169f434220464590e155f9500607745d
Ny Kanto RANDRIAMBOLOLONA <120046452+ny-kanto@users.noreply.github.com>
2026-02-22 09:51:14 +0300

    produit user

--- routes/produit.routes.js
+++ routes/produit.routes.js
@@ -12,15 +12,8 @@ const router = express.Router();
 
 router.get('/boutique/:boutiqueId', getProductsByBoutique);
 router.get('/:id', findProduitById);
-router.post('/', (req, res, next) => {
-    console.log('Route POST /produits appelée');
-    next();
-}, upload.array('images', 5), (req, res, next) => {
-    console.log('Multer a terminé, passage au controller');
-    next();
-}, createProduit);
-
-router.put('/:id', updateProduit);
+router.post('/', upload.array('images', 5), createProduit);
+router.put('/:id', upload.array('images', 5), updateProduit);
 router.delete('/:id', deleteProduit);
 
 export default router;
