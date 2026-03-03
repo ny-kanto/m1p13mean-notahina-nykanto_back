c2e1f179291977bc41b8e0c083bb951b1b0c5f3b
Ny Kanto RANDRIAMBOLOLONA <120046452+ny-kanto@users.noreply.github.com>
2026-03-03 19:48:49 +0300

    route acces

--- routes/avis.routes.js
+++ routes/avis.routes.js
@@ -2,6 +2,7 @@
 import express from "express";
 import auth from "../middlewares/auth.js";
 import { upsertAvis, deleteAvis, getAvisForEntity } from "../controllers/avis.controller.js";
+import { checkRole } from "../middlewares/role.js";
 
 const router = express.Router();
 
@@ -9,7 +10,7 @@ const router = express.Router();
 router.get("/:entityType/:entityId", getAvisForEntity);
 
 // Protected (écriture)
-router.post("/", auth, upsertAvis);
-router.delete("/:id", auth, deleteAvis);
+router.post("/", auth, checkRole('acheteur'), upsertAvis);
+router.delete("/:id", auth, checkRole('acheteur'), deleteAvis);
 
 export default router;
--- routes/produit.routes.js
+++ routes/produit.routes.js
@@ -6,14 +6,16 @@ import {
     updateProduit,
     deleteProduit,
 } from '../controllers/produit.controller.js';
+import auth from '../middlewares/auth.js';
 import upload from '../middlewares/upload.js';
+import { checkRole } from '../middlewares/role.js';
 
 const router = express.Router();
 
-router.get('/boutique/:boutiqueId', getProductsByBoutique);
-router.get('/:id', findProduitById);
-router.post('/', upload.array('images', 5), createProduit);
-router.put('/:id', upload.array('images', 5), updateProduit);
-router.delete('/:id', deleteProduit);
+router.get('/boutique/:boutiqueId', auth, checkRole('acheteur', 'boutique'), getProductsByBoutique);
+router.get('/:id', auth, checkRole('acheteur', 'boutique'), findProduitById);
+router.post('/', auth, checkRole('boutique'), upload.array('images', 5), createProduit);
+router.put('/:id', auth, checkRole('boutique'), upload.array('images', 5), updateProduit);
+router.delete('/:id', auth, checkRole('boutique'), deleteProduit);
 
 export default router;
